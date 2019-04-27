ff21ac82e730ad8fad016c8abbeb81b50d79dcd7
IareDave <gouletdavid95@gmail.com>
2019-04-26 21:10:51 -0400

    Add AWS Functionality - AWS config setup - s3 buckets linked to backend API - S3 buckets lacks upload photo functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'uploads/new'
+  get 'uploads/create'
+  get 'uploads/index'
   # RESTful routes
   resources :examples, except: %i[new edit]
-
+  resources :uploads
   # Custom routes
+  root 'uploads#new'
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out' => 'users#signout'
