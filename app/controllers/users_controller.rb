495f3b8a2287bfaec93e66a2161ecc75c3fe9d58
iaredave <gouletdavid95@gmail.com>
2019-04-27 23:18:16 -0400

    Set jQuery - Added jQuery file upload plugin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@
 
 class UsersController < ProtectedController
   skip_before_action :authenticate, only: %i[signup signin]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
 
   # POST '/sign-up'
   def signup
@@ -47,6 +49,14 @@ class UsersController < ProtectedController
   end
 
   private
+
+  def set_s3_direct_post
+    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_creds
     params.require(:credentials)
