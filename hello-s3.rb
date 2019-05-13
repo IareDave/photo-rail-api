b668b2648c638906e26ff9fa02b03a6a46c1142f
IareDave <gouletdavid95@gmail.com>
2019-05-13 05:04:24 -0400

    Delete aws key

--- hello-s3.rb
+++ hello-s3.rb
@@ -6,6 +6,3 @@ Aws.config[:credentials] = Aws::Credentials.new()
 s3 = Aws::S3::Client.new
 resp = s3.list_buckets
 resp.buckets.map(&:name)
-
-export AWS_ACCESS_KEY_ID=AKIAVOCGRFS7OLYIFME2
-export AWS_SECRET_ACCESS_KEY=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
